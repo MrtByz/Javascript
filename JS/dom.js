7c4053960d055d0b001e4b2c3d6a6ef3757a023d
https://intern.obss.com.tr/bitbucket <mrtbeyz@gmail.com>
2023-07-25 20:00:17 +0300

    DOM Giriş

---
+++ JS/dom.js
@@ -0,0 +1,12 @@
+// pathname
+console.log("Doküman pathi", document.location.pathname)
+
+// document head
+console.log(document.head)
+
+// document body
+console.log(document.body)
+
+// change background color of body
+// bu değişiklikleri css yerine jste yapma sebebş
+document.body.style.backgroundColor = "green"
