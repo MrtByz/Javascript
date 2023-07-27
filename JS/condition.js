f0f738a7fb51d93fa6d0b62ba1254ebb992dee30
https://intern.obss.com.tr/bitbucket <mrtbeyz@gmail.com>
2023-07-27 20:15:02 +0300

    Mantıksal operatörler konusu işlendi

---
+++ JS/condition.js
@@ -0,0 +1,38 @@
+// koşul ifadelerinin kullanımı bu js dosyasında yer almaktadır
+
+// Mantıksal operatörler
+
+// değerlerin eşitliği
+var a = 10;
+var b = "10";
+console.log("a=10 b='10' a==b", a==b) // true
+
+// hem değerler hem türler eşitse
+var c = 10;
+var d = "10";
+console.log("c=10 d='10' c===d", c===d) // false
+
+// eşit değilse
+console.log("a=10 b='10' a!=b", a != b) // false
+console.log("a=10 b='10' a!==b", a!==b) // true
+
+// küçükse
+var f = 10;
+var g = 10;
+console.log("f=10 g=10 f<g", f<g) // false
+
+// ve &&
+var h = 10;
+var j = "kodluyoruz";
+console.log("h>10 && j==kodluyoruz", h>11 && j=="kodluyoruz") // false
+
+// veya ||
+console.log("h>10 || j==kodluyoruz", h>11 || j=="kodluyoruz") // true
+
+
+
+
+
+
+
+
