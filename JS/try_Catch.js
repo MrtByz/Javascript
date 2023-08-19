bf0d308a92a0cce7f835e42e6eb788be4c7cf49a
Murat BEYAZ <51954423+MrtByz@users.noreply.github.com>
2023-08-19 21:56:03 +0300

    Add files via upload
    Try Catch Throw öğrenildi.

---
+++ JS/try_Catch.js
@@ -0,0 +1,10 @@
+try {
+    //Çalıştırılacak kodlar
+}catch(hata) {
+    //Hata yakalandığında çalışacak kodlar
+}finally {
+    //try tamamlandıktan sonra hata olsada olmasada çalışacak olan kodlar.
+}
+
+
+throw 500 // throw ifadesi bize özel hatalarımızı oluşturmanıza olanak tanır. Teknik olarak throw ile bir istisna (hata) atılabilir.
